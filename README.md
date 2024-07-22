42f9e65619df9343c0fd6680c78aa1310e4db2b6
AdamLesage <adamles44@gmail.com>
2024-07-22 19:10:22 +0200

    feat(Agent image prediction): can question agent on which cancer is on picturef

--- README.md
+++ README.md
@@ -9,7 +9,14 @@ This project aims to develop an image recognition system using machine learning 
 3. Install the dependencies by running the following command:
 
 ```shell
+pip install keras h5py keras-resnet
 python3 src/install.py
 ```
 
-4. Then enter tour kaggle username and your kaggle key to download the dataset.
+### Libraries explanation
+
+- `keras` is a high-level neural networks API, written in Python and capable of running on top of TensorFlow, CNTK, or Theano.
+- `h5py` is a common package to interact with a dataset that is stored on an H5 file.
+- `keras-resnet` is a library that provides pre-trained ResNet models.
+
+1. Then enter your kaggle username and your kaggle key to download the dataset.
--- src/CNN_model_training.py
+++ src/CNN_model_training.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 import os
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
 # Load the data
 def load_data(data_dir) -> tuple:
@@ -42,3 +44,45 @@ print(f"X_train_glioma shape: {x_train_glioma.shape}")
 print(f"X_test_glioma shape: {x_test_glioma.shape}")
 print(f"Y_train_glioma shape: {y_train_glioma.shape}")
 print(f"Y_test_glioma shape: {y_test_glioma.shape}")
+
+model = Sequential()
+
+# Add the layers to the model
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3))) # 256x256x3 because the images are 256x256 pixels and have 3 channels
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+# Summary of the model
+model.summary()
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(x_train_glioma, y_train_glioma, epochs=10, validation_data=(x_test_glioma, y_test_glioma))
+
+# Plot the accuracy and loss
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.legend()
+plt.show()
+
+score = model.evaluate(x_test_glioma, y_test_glioma, verbose=0)
+print(f"Test loss: {score[0]}")
+print(f"Test accuracy: {score[1]}")
+
+# Save the model
+model.save('brain_tumor_cnn.h5')
